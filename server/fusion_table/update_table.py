f99d6016ea522f8207e420c1bfac3e2f3e908e15
Michael Genkin <misha.genkin@gmail.com>
2011-06-04 15:36:38 +0300

    Made the extractor a bit more intelligent

--- server/fusion_table/update_table.py
+++ server/fusion_table/update_table.py
@@ -15,7 +15,5 @@ def insert_row( data ):
 	token = ClientLogin().authorize("misha.genkin@gmail.com", "Mch@Gn88")
 	ft_client = ftclient.ClientLoginFTClient(token)
 	
-	#print SQL().insert(_TID, data)
-	#rowid = int(ft_client.query(SQL().insert(_TID, data)).split("\n")[1])
 	query = "INSERT INTO 946168 (id, type, status, text, followup, link, report, unit, topic, office) VALUES (%d, %d, %d, '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
 	ft_client.query(query % data)
